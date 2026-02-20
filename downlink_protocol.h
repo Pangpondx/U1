17f8929f06238be85805d42668b8577b8caf95d9
Pangpondx <156653875+Pangpondx@users.noreply.github.com>
2026-02-20 17:55:28 +0700

    โค้ดเก่าใช้ได้

---
+++ downlink_protocol.h
@@ -0,0 +1,195 @@
+#pragma once
+#include <px4_platform_common/module.h>
+#include <px4_platform_common/log.h>
+#include <px4_platform_common/px4_work_queue/ScheduledWorkItem.hpp>
+// #include <uORB/Publication.hpp>
+// #include <uORB/topics/actuator_servos.h>
+#include <uORB/Subscription.hpp>
+#include <uORB/topics/airspeed.h>
+#include <uORB/topics/disco_data.h>
+#include <uORB/topics/sensor_combined.h>
+#include <uORB/topics/vehicle_air_data.h>
+#include <uORB/topics/vehicle_angular_velocity.h>
+#include <uORB/topics/vehicle_attitude.h>
+#include <uORB/topics/vehicle_global_position.h>
+#include <uORB/topics/vehicle_local_position.h>
+#include <uORB/topics/vehicle_status.h>
+#include <uORB/topics/wind.h>
+#include <matrix/matrix/math.hpp>
+
+#define UART_PORT "/dev/ttyS2"
+#define UART_BAUDRATE B115200
+
+#define HI_BYTE 0x5A
+#define LOW_BYTE 0xA5
+#define SRC_ID 0x38
+#define DES_ID 0xFF
+#define PACKET_ID_TELEMETRY 0x80
+#define UAV_CTRL 0x7E
+#define PACKET_ID_ENGINE 0x81
+#define PACKET_ID_TEMPERATURE 0x82
+#define PACKET_ID_ACKNOWLEDGE 0xAA
+#define PACKET_ID_CONTROL 0x84
+
+typedef struct __attribute__((packed)){
+	uint8_t packet_id;
+	uint8_t length_in_bytes;
+	uint32_t tailed_number;
+	uint8_t flight_mode;
+	// uint32_t air_speed;
+	// uint32_t angle_of_attack;
+	// uint32_t angle_of_sideslip;
+	// uint32_t roll_angle;
+	// uint32_t pitch_angle;
+	// uint32_t yaw_angle;
+	// uint32_t roll_rate;
+	// uint32_t pitch_rate;
+	// uint32_t yaw_rate;
+	// uint32_t wind_speed;
+	// uint32_t wind_direction;
+	float air_speed;
+	float angle_of_attack;
+	float angle_of_sideslip;
+	float roll_angle;
+	float pitch_angle;
+	float yaw_angle;
+	float roll_rate;
+	float pitch_rate;
+	float yaw_rate;
+	float wind_speed;
+	float wind_direction;
+	uint32_t spare_1 : 29;
+	uint32_t disengage_ias : 1;
+	uint32_t disengage_altitute : 1;
+	uint32_t disengage_heading : 1;
+	// uint32_t accelerometer_x;
+	// uint32_t accelerometer_y;
+	// uint32_t accelerometer_z;
+	// uint32_t velocity_north;
+	// uint32_t velocity_east;
+	// uint32_t velocity_down;
+	// uint32_t latitude;
+	// uint32_t longitude;
+	// uint64_t gps_height;
+	// uint64_t baro_altitude;
+	float accelerometer_x;
+	float accelerometer_y;
+	float accelerometer_z;
+	float velocity_north;
+	float velocity_east;
+	float velocity_down;
+	float latitude;
+	float longitude;
+	double_t gps_height;
+	double_t baro_altitude;
+	uint8_t uav_controled_by_id : 7;
+	uint8_t spare_2 : 1;
+} DOWNLINK_TELEMETRY;
+
+typedef struct __attribute__((packed)){
+	uint8_t packet_id;
+	uint8_t length_in_bytes;
+	uint16_t engine_rpm;
+	uint16_t injection_1_time;
+	uint16_t injection_2_time;
+	uint16_t throttle_position;
+	uint16_t barometric;
+	uint16_t battery_voltage;
+	uint16_t cht_l;
+	uint16_t cht_r;
+	uint16_t egt_l;
+	uint16_t egt_r;
+	uint16_t fuel_pressure;
+	uint16_t air_temp;
+	uint16_t duty_pump;
+	uint8_t smot;
+	uint8_t ignition_1;
+	uint8_t ignition_2;
+	uint8_t ignition_3;
+	uint8_t ignition_4;
+} DOWNLINK_ENGINE_STATUS;
+
+typedef struct __attribute__((packed)){
+	uint8_t packet_id;
+	uint8_t length_in_bytes;
+	int8_t outside_air_temp;
+	int8_t fcc_temperature;
+	uint8_t fcc_humidity;
+} DOWNLINK_TEMPERATURE_REPORT;
+
+typedef struct __attribute__((packed)){
+	uint8_t packet_id;
+	uint8_t length_in_bytes;
+	uint8_t acknowledge_packet_id;
+	uint8_t sequence_number;
+	uint8_t response_code;
+} DOWNLINK_ACKNOWLEDGE;
+
+typedef struct __attribute__((packed)){
+	uint8_t packet_id;
+	uint8_t length_in_bytes;
+	int16_t flap_l;
+	int16_t flap_r;
+	int16_t aileron_l;
+	int16_t aileron_r;
+	int16_t elevator_l;
+	int16_t elevator_r;
+	int16_t rudder_l;
+	int16_t rudder_r;
+	int16_t throttle;
+	int16_t nose_wheel_angle;
+	uint8_t break_ctrl;
+} DOWNLINK_CONTROL_SURFACE;
+
+typedef struct __attribute__((packed)){
+	uint8_t protocol_iden_hi;
+	uint8_t protocol_iden_low;
+	uint8_t protocol_version;
+	uint8_t source_id : 7;
+	uint8_t take_control : 1;
+	uint8_t destination_id;
+	uint8_t sequence;
+	uint16_t length;
+	uint32_t unix_time_stamp;
+	uint32_t utc_microsecond_time_stamp;
+	uint8_t header_check;
+	//user_data//
+	DOWNLINK_TELEMETRY telemetry_data;
+	DOWNLINK_ENGINE_STATUS engine_data;
+	DOWNLINK_TEMPERATURE_REPORT temperature_data;
+	DOWNLINK_ACKNOWLEDGE acknowledge_data;
+	DOWNLINK_CONTROL_SURFACE control_data;
+	uint16_t checksum;
+} MESSAGE_HEADER;
+
+class Downlink : public ModuleBase<Downlink>, public px4::ScheduledWorkItem
+{
+public:
+	Downlink();
+	~Downlink() override;
+	static Downlink *instantiate(int argc,char *argv[]);
+	static int task_spawn(int argc,char *argv[]);
+	static int custom_command(int argc,char *argv[]);
+	static int print_usage(const char *reason = nullptr);
+private:
+	bool Init();
+	void Run() override;
+	int open_uart();
+	uint8_t cal_header_check(const uint8_t* pkt);
+	uint16_t GetCRC16(const void *buf, unsigned len);
+	static const uint16_t crc16tab[256];
+	int fd{-1};
+	MESSAGE_HEADER msg;
+
+	//uORB::Publication<actuator_servos_s> act_servo_pub{ORB_ID(actuator_servos)};
+	uORB::Subscription airspeed_sub{ORB_ID(airspeed)};
+	uORB::Subscription disco_data_sub{ORB_ID(disco_data)};
+	uORB::Subscription sensor_sub{ORB_ID(sensor_combined)};
+	uORB::Subscription air_data_sub{ORB_ID(vehicle_air_data)};
+	uORB::Subscription angular_velocity_sub{ORB_ID(vehicle_angular_velocity)};
+	uORB::Subscription attitude_sub{ORB_ID(vehicle_attitude)};
+	uORB::Subscription global_pos_sub{ORB_ID(vehicle_global_position)};
+	uORB::Subscription local_pos_sub{ORB_ID(vehicle_local_position)};
+	uORB::Subscription status_sub{ORB_ID(vehicle_status)};
+	uORB::Subscription wind_sub{ORB_ID(wind)};
+};
